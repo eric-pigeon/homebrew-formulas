aa8075325e6c84c76ebae2f377a0e21069d99146
Eric Pigeon <epigeon@weblinc.com>
2018-01-09 16:05:15 -0500

    test

---
+++ kafka@0.7.rb
@@ -0,0 +1,66 @@
+class KafkaAT07 < Formula
+  desc "Publish-subscribe messaging rethought as a distributed commit log"
+  homepage "https://kafka.apache.org/"
+  url "https://dl.bintray.com/eric-pigeon/mirror/bottles/kafka%400.7-0.7.2.high_sierra.bottle.tar.gz"
+  sha256 "ac599982a3f4b368c0d76dd399da4522785cd94de0c1b36fc2a2deb22df9f7ef"
+
+  bottle do
+    cellar :any_skip_relocation
+    root_url "https://dl.bintray.com/eric-pigeon/mirror/bottles"
+    sha256 "ac599982a3f4b368c0d76dd399da4522785cd94de0c1b36fc2a2deb22df9f7ef" => :high_sierra
+  end
+
+  depends_on "zookeeper"
+  depends_on :java => "1.8"
+
+  def install
+    data = var/"lib"
+    inreplace "config/server.properties",
+      "log.dirs=/tmp/kafka-logs", "log.dirs=#{data}/kafka-logs"
+
+    inreplace "config/zookeeper.properties",
+      "dataDir=/tmp/zookeeper", "dataDir=#{data}/zookeeper"
+
+    libexec.install "libs"
+
+    prefix.install "bin"
+    bin.env_script_all_files(libexec/"bin", Language::Java.java_home_env("1.8"))
+    Dir["#{bin}/*.sh"].each { |f| mv f, f.to_s.gsub(/.sh$/, "") }
+
+    mv "config", "kafka"
+    etc.install "kafka"
+    libexec.install_symlink etc/"kafka" => "config"
+
+    # create directory for kafka stdout+stderr output logs when run by launchd
+    (var+"log/kafka").mkpath
+  end
+
+  plist_options :manual => "zookeeper-server-start #{HOMEBREW_PREFIX}/etc/kafka/zookeeper.properties & kafka-server-start #{HOMEBREW_PREFIX}/etc/kafka/server.properties"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>WorkingDirectory</key>
+        <string>#{HOMEBREW_PREFIX}</string>
+        <key>ProgramArguments</key>
+        <array>
+            <string>#{opt_bin}/kafka-server-start</string>
+            <string>#{etc}/kafka/server.properties</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/kafka/kafka_output.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/kafka/kafka_output.log</string>
+    </dict>
+    </plist>
+    EOS
+  end
+end
